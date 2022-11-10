ebb75e3600c577b1e28c09c69b06a260c6c06852
oshans <oshans@timbertruss.com.au>
2022-11-10 16:14:58 +1100

    2

--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing123", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Baldfffdffdfmye", "Hot", "eeee", "edited again11"
+        "Freezing123", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Baldfffdffdfmye", "Hot", "eeee", "edited again1122"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
